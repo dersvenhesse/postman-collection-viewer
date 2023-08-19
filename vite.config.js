5170bfd3ef6bbc1361a048c419dc8e4c0f3a2524
dersvenhesse <dersvenhesse@users.noreply.github.com>
2023-08-19 11:50:52 +0200

    ci: add workflow for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import {fileURLToPath, URL} from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/postman-collection-viewer/",
   plugins: [
     vue({
       template: {transformAssetUrls}
